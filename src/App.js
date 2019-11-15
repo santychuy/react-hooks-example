66a7507ed3cfd785a764b9e7c3e512b8bcfda956
santychuy <jsanticc@hotmail.com>
2019-11-14 17:07:10 -0700

    Check useContext

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useContext } from 'react';
 import './App.css';
 import { useForm } from './hooks/useForm';
 import { useFetch } from './hooks/useFetch';
 
+//Context
+import { ExampleContext } from './hooks/context/example-context';
+
 const App = () => {
   const [{ email, password, firstName }, handleChanges] = useForm({ email: '', password: '', firstName: '' });
-
   const [count, setCount] = useState(0);
+
   const {data} = useFetch(`http://numbersapi.com/${count}/trivia`);
+
+  const msg = useContext(ExampleContext);
+
+  //Hacer referencia a cualquier componente y tenerla en cualquier parte de la App
+  const inputRef = useRef();
 
   /* //Component Did Mount
   useEffect(() => {
@@ -24,15 +32,23 @@ const App = () => {
   }, [email]); */
 
   return (
-    <div className="App">
-      <div>{!data ? "Loading..." : data}</div>
-      <div>Count: {count}</div>
-      <button onClick={() => setCount(c => c + 1)}>Increment</button>
+    <ExampleContext.Provider value="Hola Loco">
+      <div className="App">
+        <div>{!data ? "Loading..." : data}</div>
+        <div>Count: {count}</div>
+        <button onClick={() => setCount(c => c + 1)}>Increment</button>
 
-      <input name='email' value={email} onChange={handleChanges} placeholder='Email' />
-      <input name='firstName' value={firstName} onChange={handleChanges} placeholder='First Name' />
-      <input type='password' name='password' value={password} onChange={handleChanges} placeholder='Password' />
-    </div>
+        <input ref={inputRef} name='email' value={email} onChange={handleChanges} placeholder='Email' />
+        <input name='firstName' value={firstName} onChange={handleChanges} placeholder='First Name' />
+        <input type='password' name='password' value={password} onChange={handleChanges} placeholder='Password' />
+
+        <button onClick={() => {
+          inputRef.current.focus();
+        }}>Reference</button>
+
+        <div>HABER: {msg}</div>
+      </div>
+    </ExampleContext.Provider>
   );
 }
 
